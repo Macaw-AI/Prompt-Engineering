7df24813bd67343347a5ac7646df8b3f61079086
jakub-ner <kubaner1@gmail.com>
2023-01-15 15:18:52 +0100

    Few-shot example and results Unfinished demo of SplitedIntoTasks

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ Playground for prompt engineering research.
 <br><br>
 #### Requirements
 ```
-pip3 install openai-0.26.1
+pip install openai-0.26.1 
 ```
+
+### Issues to resolve:
+- [ ] If user can't find proper word, model should help him
+- [ ] Model should be focused on the user, lesson subject is secondary
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import openai
 # import openai_secret_manager
 
-from .FewShotDaVinci import prompt, Teacher, Student, Subject
+from FewShot import prompt, Teacher, Student, Subject
+# from SplitedIntoTasks import prompt, Teacher, Student, Subject
 
 # secrets = openai_secret_manager.get_secret("openai")
 # api_key = secrets["api_key"]
@@ -10,18 +11,20 @@ from .FewShotDaVinci import prompt, Teacher, Student, Subject
 api_key = "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"
 openai.api_key = api_key
 
-while True:
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.8,
-        max_tokens=150,
-        n=1,
-        # frequency_penalty=0,
-        # presence_penalty=0.6,
-        stop=[f"{Teacher.name}:", f"{Student.name}:"],
+if __name__ == '__main__':
+    while True:
+        response = openai.Completion.create(
+            engine="text-curie-001",
+            prompt=prompt,
+            temperature=0.8,
+            max_tokens=150,
+            n=1,
+            # frequency_penalty=0,
+            # presence_penalty=0.6,
+            stop=[f"{Teacher.name}:", f"{Student.name}:"],
 
-    )
-    print(prompt + response.choices[0].text)
-    user_response = f"\n{Student.name}: {input(f'{Student.name}: ')}\n{Teacher.name}: "
-    prompt += response.choices[0].text + user_response
+        )
+        print(prompt + response.choices[0].text[1:])
+        user_response = f"{Student.name}: {input(f'{Student.name}: ')}\n{Teacher.name}: "
+        prompt += response.choices[0].text + user_response
+
